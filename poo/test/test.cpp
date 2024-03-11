3c45c53666bbd5c47c79b18a7afb8c747edc758f
Moscalu <moscalu.daniel.vd@gmail.com>
2024-03-11 16:52:26 +0200

    Created test directory and deleted junk file.

---
+++ poo/test/test.cpp
@@ -0,0 +1,114 @@
+// 1.Lab
+// Elaborati clasa: Vehicul - serie nr
+// care sa contina un identificator(exemplu mai sus) important e sa
+// fie unic.
+// Sa contina cel putin 3 caracteristici(an, brand, model)
+// Sa contina cel putin un constructor(implicit, cu parametri, de copiere)
+// Sa contina metode de setare (set, get)
+// metoda de validare a identificatorului
+
+// Exemplificati utilizarea clasei prin creearea mai multor obiecte, validarea identificatorilor si apelul metodelor.
+
+// algoritmul de validare a IDNV(incepe cu 3 si ultima cifra de control)
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+private:
+    string identifier;
+    int year;
+    string brand;
+    string model;
+
+public:
+    // // Constructors
+    // Vehicle() : identifier(""), year(0), brand(""), model("") {}
+    // Vehicle(string id, int y, string b, string m) : identifier(id), year(y), brand(b), model(m) {}
+    // Vehicle(const Vehicle& other) : identifier(other.identifier), year(other.year), brand(other.brand), model(other.model) {}
+
+    // Getters and setters
+    void setIdentifier(string id) {
+        identifier = id;
+    }
+
+    string getIdentifier() const {
+        return identifier;
+    }
+
+    // void setYear(int y) {
+    //     year = y;
+    // }
+
+    // int getYear() const {
+    //     return year;
+    // }
+
+    // void setBrand(string b) {
+    //     brand = b;
+    // }
+
+    // string getBrand() const {
+    //     return brand;
+    // }
+
+    // void setModel(string m) {
+    //     model = m;
+    // }
+
+    // string getModel() const {
+    //     return model;
+    // }
+
+    // // Method to validate identifier
+    // bool validateIdentifier() const {
+    //     // Assuming the identifier should start with '3' and end with a control digit
+    //     if (identifier.length() != 13) {
+    //         return false;
+    //     }
+
+    //     // Check the first character
+    //     if (identifier[0] != '3') {
+    //         return false;
+    //     }
+
+    //     // Check the last character as a control digit
+    //     // 3+0+1+2+3+4+5+6+7+8+9+0 = 48 % 10 = 8
+    //     int sum = 0;
+    //     for (int i = 0; i < 12; ++i) {
+    //         sum += (identifier[i] - '0');
+    //     }
+    //     int controlDigit = sum % 10;
+
+    //     return (controlDigit == (identifier[12] - '0'));
+    // }
+};
+
+int main() {
+
+    Vehicle exampleObject1;
+    exampleObject1.setIdentifier("123456789");
+    cout << exampleObject1.getIdentifier();
+
+    // // Example usage
+    // Vehicle car1("3012345678908", 2022, "Toyota", "Camry");
+    // Vehicle car2("3123456789354", 2023, "Honda", "Accord");
+
+    // // Validate identifiers
+    // if (car1.validateIdentifier()) {
+    //     cout << "Car 1 identifier is valid." << endl;
+    // } else {
+    //     cout << "Car 1 identifier is invalid." << endl;
+    // }
+
+    // if (car2.validateIdentifier()) {
+    //     cout << "Car 2 identifier is valid." << endl;
+    // } else {
+    //     cout << "Car 2 identifier is invalid." << endl;
+    // }
+
+    
+
+    return 0;
+}
