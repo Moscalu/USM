93746e011bf8b6e5a152cc05cfe53bc33ad6489a
Moscalu <moscalu.daniel.vd@gmail.com>
2024-03-28 22:24:05 +0200

    Formatted code for exam1.

--- javaScript/atestari/1st/atestare1.js
+++ javaScript/atestari/1st/atestare1.js
@@ -1,6 +1,5 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
+const fieldsValueCounter = document.getElementById('calorie-counter');
+const fieldsInput = document.getElementById('budget');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
@@ -17,53 +16,33 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  //const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const targetInputContainer = document.querySelector('#raw .input-container')
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="number"]').length + 1;
   const HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <label for="raw-${entryNumber}-value">Entry ${entryNumber} Value</label>
   <input
     type="number"
     min="0"
-    id="${entryDropdown.value}-${entryNumber}-calories"
-    placeholder="Calories"
-  />`;
+    id="raw-${entryNumber}-value"
+    placeholder="Value"
+  /><br>`;
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
-function calculateCalories(e) {
+function calculateValues(e) {
   e.preventDefault();
   isError = false;
 
-  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
-  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
-  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
-  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
-  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
-
-  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
-  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
-  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
-  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
-  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  const rawNumberInputs = document.querySelectorAll('#raw input[type=number]');
+  const rawValues = getCaloriesFromInputs(rawNumberInputs);
 
   if (isError) {
     return;
   }
 
-  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
-  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-  output.innerHTML = `
-  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
-  <hr>
-  <p>${budgetCalories} Calories Budgeted</p>
-  <p>${consumedCalories} Calories Consumed</p>
-  <p>${exerciseCalories} Calories Burned</p>
-  `;
-
+  const rawValuesSum = rawValues;
+  output.innerHTML = `<p>Sum of current fields is: ${rawValuesSum}</p>`;
   output.classList.remove('hide');
 }
 
@@ -91,10 +70,12 @@ function clearForm() {
     container.innerHTML = '';
   }
 
-  budgetNumberInput.value = '';
+  fieldsInput.value = '';
   output.innerText = '';
+  output.classList.add('hide');
 
 }
 
 addEntryButton.addEventListener("click", addEntry);
-calorieCounter.addEventListener("submit", calculateCalories);
+fieldsValueCounter.addEventListener("submit", calculateValues);
+clearButton.addEventListener("click", clearForm);
